135b96ff45b5fe8339bec5b767e12aaafd132358
Sumit <sumityadav0376@gmail.com>
2022-02-02 12:04:58 +0530

    Run first ts-node server

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,17 @@
-const greetings: string = 'Hello';
+import express, { Application, Request, Response, NextFunction } from 'express';
 
-const numbers: number[] = [1, 2, 3];
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+const add = (a: number, b: number): number => a+b;
+
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+    console.log(add(5, 5));
+    res.send("Hey!!! It's running fine...");
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port : ${port}`);
+}).on("error", (err) => {
+    console.log(err);
+})
